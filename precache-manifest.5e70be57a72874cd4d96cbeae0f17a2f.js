bbfcd0338008ebfba25f510f044e5f884b6222df
shanyiming <shanyiming@hotmail.com>
2019-09-01 00:32:47 -0700

    Updates

---
+++ precache-manifest.5e70be57a72874cd4d96cbeae0f17a2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2f3109ccdc4612949d07e4dfdd85f977",
+    "url": "/react/index.html"
+  },
+  {
+    "revision": "1fd393666b2bdd82d69d",
+    "url": "/react/static/css/2.5a2f7233.chunk.css"
+  },
+  {
+    "revision": "061b1fae1042f4d58725",
+    "url": "/react/static/css/main.c332a520.chunk.css"
+  },
+  {
+    "revision": "1fd393666b2bdd82d69d",
+    "url": "/react/static/js/2.f2e2e5ae.chunk.js"
+  },
+  {
+    "revision": "061b1fae1042f4d58725",
+    "url": "/react/static/js/main.39ced74b.chunk.js"
+  },
+  {
+    "revision": "4e6a4b677d1c1e6ba359",
+    "url": "/react/static/js/runtime~main.bb50a20d.js"
+  },
+  {
+    "revision": "0e80f63b08b6d5852949915fdde6bdaa",
+    "url": "/react/static/media/logo.0e80f63b.png"
+  }
+]);
